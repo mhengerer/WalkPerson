af668f030ba748c4db8eeb9e5d400da32decb26a
unknown <lexiarbajianclt@gmail.com>
2022-11-25 20:37:07 -0500

    GoogleMaps API JS for user location and route duration

--- script-route-duration.js
+++ script-route-duration.js
@@ -2,65 +2,19 @@
 
 var apiKey = "AIzaSyDjEs-V1HGJ2sUwaWta6FhTRjTmAyoNxd4";
 
-var destination = 
-
-
-
-function getLocation() {
-    document.getElementById(
-        "buttonID").disabled = true;
-
-    document.getElementById(
-        "buttonID").innerHTML = "Executing..";
-
-    if ("geolocation" in navigator) {
-        navigator.geolocation
-        .getCurrentPosition(function (position) {
-            var currentLatitude 
-                = position.coords.latitude;
-
-            var currentLongitude 
-                = position.coords.longitude;
-
-            var infoLatLang = "Latitude: " 
-                + currentLatitude +
-                "<br>Longitude: " + 
-                currentLongitude;
-
-            var infoContent = 
-                new google.maps.InfoWindow
-                ({ map: map, content: infoLatLang });
-
-            var currentLocation = {
-                lat: currentLatitude,
-                lng: currentLongitude
-            };
-
-            infoContent.setPosition(
-                    currentLocation);
-                      
-            document.getElementById("buttonID")
-                .style.display = 'none';
-        });
-    }
-}// end function getLocation()
-
-//--------------------usable data for locating user
-
-// tracks user 
 x = navigator.geolocation;
 x.getCurrentPosition(getUserLocation);
 
 var userLat = position.coords.latitiude; //keep - finds user lat
 var userLong = position.coords.longitude; //keep - finds user long
-var userLocation = [userLat, userLong];
+var userLocation = [userLat, userLong]; //use for origin in route duration - merges lat and long into usable data for user origin
 
 
  document.getElementById("button").addEventListener("click", routeDurationCalc); //event listener for when button is pressed to find user 
 
 function routeDurationCalc () {
 
-    var routeData = {  //use for origin in route duration - merges lat and long into usable data for user origin
+    var routeData = {  
 
         origin: userLocation,
         destination: document.getElementById("dest-input").value,
@@ -80,19 +34,5 @@ function routeDurationCalc () {
     })
 
 }
-
-//event listener for when button is pressed to find user 
-
-
-
-var userLocation = [userLat + userLong]; //use for origin in route duration - merges lat and long into usable data for user origin
-
-
-
-
-//route duration 
-
-//par - origin and dest 
-
 
 
