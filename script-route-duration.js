b88afd4ed8d0e8dde6265f6be3201b68bda09f4b
unknown <lexiarbajianclt@gmail.com>
2022-11-27 16:20:43 -0500

    correct gm API with initMap callback function - function to render map in window

--- script-route-duration.js
+++ script-route-duration.js
@@ -8,12 +8,12 @@ x.getCurrentPosition();
 var userLat = position.coords.latitiude; //keep - finds user lat
 var userLong = position.coords.longitude; //keep - finds user long
 var userLocation = [userLat, userLong]; //use for origin in route duration - merges lat and long into usable data for user origin
+var userDestination = document.getElementById("dest-input").value;
 
 
+ document.getElementById("submitButton").addEventListener("click", initMap); //event listener for when button is pressed to find user 
 
- document.getElementById("submitButton").addEventListener("click", routeDurationCalc); //event listener for when button is pressed to find user 
-
-function routeDurationCalc () {
+function initMap () {
 
     var routeData = {  
 
@@ -29,35 +29,38 @@ function routeDurationCalc () {
 
                 const duration = document.querySelector("#duration");
                 duration.innerHTML = "Your trip is " + result.routes[0].legs[0].distance.text + "long and will take " + result.routes[0].legs[0].duration.text + "!";
-
-            } else {
-                
             }
 
-    })
 
+
+
+        }) 
 }
 
-var mapsetting = {
 
-    center: myLatLng,
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-    
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(userLocation, userDestination),
+  zoom:5,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
 };
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
 
-/*
+renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDjEs-V1HGJ2sUwaWta6FhTRjTmAyoNxd4Y&callback=initMap");
+    window.initMap = this.initMap;
+  }
+}
 
-var mapsetting ={
-    center: myLatLng,
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-};
+
+
+
+
  // map should already be linked through HTML
 
  // insert direction service object use root method to get result 
- var directionService = new google.maps.maps.directionService();
+ var directionService = new google.maps.maps.DirectionService();
 
  // direction render object used to display the root 
  var directionsRenderer = new google.maps.DirectionsRenderer();
@@ -66,19 +69,14 @@ var mapsetting ={
  directionsDisplay.setMap(Map);
  
 
-//link the request to the original method (root)
-directionService.route(request,(result, status)=>{
-    if (status=== google.maps.directionStatus.Ok){
-        // display the route and get distance and time
-        const output = document.QuerySelector('#output');
-        output.innerHTML = "< div class = 'alert-info '> From:"+document.getElementById('Start').value +".<br /> End:"+ document.getElementById('End').value + '<br / > Walking Distance:' + result.routes[0].legs[0].distance.text +".<br /> Duration:" + result.routes[0].legs[0].duration.text +".</div>";
+var mapsetting = {
 
-        directionsDisplay.setDirections(result);
-     } else{
-        directionsDisplay.setDirections({routes:[]});
+    center: myLatLng,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
 
-    }
-}
+};
 
-*/
+
+
 
